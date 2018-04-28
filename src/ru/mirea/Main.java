04f2c66d5bdac23c539a3d13b69125559897476c
Luna <Atisphatis@gmail.com>
2018-04-28 15:17:29 +0300

    Interpretation of command line
    Реализован прототип интерпритатора командной строки.

--- src/ru/mirea/Main.java
+++ src/ru/mirea/Main.java
@@ -1,8 +1,17 @@
-package com.company;
+package ru.mirea;
+
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean[] flags = new boolean[1];
+        ArrayList<String> files = new ArrayList<>();
+        Archivator.checkCommandLine(args, flags, files);
+
+        /*System.out.println(flags[0]);
+        for (int i = 0; i < files.size(); i++){
+            System.out.println(files.get(i));
+        }*/
     }
 }
